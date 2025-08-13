f246deb0a261be905f9654bb9b249610c2a1c4be
Kawan <kawan-silva@outlook.com>
2025-08-13 17:41:41 -0300

    inserir validação

--- src/main/java/br/com/kawan/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/kawan/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,15 +2,26 @@ package br.com.kawan.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class CandidateEntity {
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "o campo [username] não deve conter espaço.")
     private String username;
+
+    @Email(message = "e campo [email] deve conter um e-mail válido.")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
+    
     private String description;
     private String curriculum;
 }
