dee271776f970bb5deddc505504c1cf653087f46
Kawan <kawan-silva@outlook.com>
2025-08-25 00:28:48 -0300

    useCase

--- src/main/java/br/com/kawan/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/kawan/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,28 +1,29 @@
 package br.com.kawan.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.kawan.gestao_vagas.exceptions.UserFoundException;
 import br.com.kawan.gestao_vagas.modules.candidate.CandidateEntity;
-import br.com.kawan.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.kawan.gestao_vagas.modules.candidate.useCases.CreateCandidateUserCase;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
     @Autowired
-    private CandidateRepository candidateRepository;
+    private CreateCandidateUserCase createCandidateUserCase;
 
     @PostMapping("/")
-    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent((user) -> {
-            throw new UserFoundException();
-        });
-
-        return this.candidateRepository.save(candidateEntity);
+    public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        try {
+            var result = this.createCandidateUserCase.execute(candidateEntity);
+            return ResponseEntity.ok().body(result);
+        } catch(Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
