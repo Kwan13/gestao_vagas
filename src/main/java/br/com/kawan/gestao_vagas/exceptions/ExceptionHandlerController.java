b7f13f3e6c0ebfe09fd6a795e526cf3cce483d9a
Kawan <kawan-silva@outlook.com>
2025-08-24 23:49:06 -0300

    criando tabela de candidato

--- src/main/java/br/com/kawan/gestao_vagas/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/kawan/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -7,7 +7,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,15 +23,10 @@ public class ExceptionHandlerController {
     public ResponseEntity<List<ErrorMessageDTO>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<ErrorMessageDTO> dto = new ArrayList<>();
 
-        e.getBindingResult().getAllErrors().forEach(err -> {
+        e.getBindingResult().getFieldErrors().forEach(err -> {
             String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
-            String field = null;
-
-            if(err instanceof FieldError) {
-                field = ((FieldError)err).getField();
-            }
-
-            ErrorMessageDTO error = new ErrorMessageDTO(message, field);
+           
+            ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
             dto.add(error);
         });
 
